3a1b54b63ef76fafd064814948389276d66a7646
tedyang1234 <98529951+tedyang1234@users.noreply.github.com>
2023-08-08 19:25:20 +0800

    Update etu.js

--- etu.js
+++ etu.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'aa0754aa-f63a-40fd-860c-7b81ca2ee399';
 
 let proxyIP = '';
 
